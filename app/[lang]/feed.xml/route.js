bd979e8709b24538fdc1c30e05f71055d023f0f1
Ezzaddin0 <ezoozm24@gmail.com>
2024-01-17 22:15:30 +0300

    1- update search. 2- fix in home page bed quality image and section heroes. 3- fix random list. 4- limait categories list. 5- add soical media account. 6- update infinte card and function. 7- add card info for page. 8- fix size image in post page. 8- add section you may like it in post pag. 9- icon website. 10- fix keywords tag and description

--- app/[lang]/feed.xml/route.js
+++ app/[lang]/feed.xml/route.js
@@ -2,10 +2,8 @@ import RSS from 'rss';
 
 import { client } from "@/lib/createClient";
 import { groq } from "next-sanity";
-import { Locale } from '@/i18n.config'
-import { getDictionary } from '@/lib/dictionary'
 
-export async function GET({lang}) {
+export async function GET() {
 
     const query = groq`
     *[_type == 'post']{
@@ -20,10 +18,10 @@ export async function GET({lang}) {
     const feed = new RSS({
         title: "aqraaz",
         description: "Discover boundless insights across diverse domains on the Aqraaz blog. Delve into a wealth of specialized content covering technology, business strategies, lifestyle trends, health advice, and more. Our curated collection of articles and resources caters to every interest and expertise level, ensuring a rewarding reading experience for all. Explore the Aqraaz blog today for expert perspectives, valuable tips, and in-depth analyses across a myriad of categories",
-        id: `https://www.aqraaz.com/${lang}/`,
-        link: `https://www.aqraaz.com/${lang}/`,
-        site_url:  `https://www.aqraaz.com/${lang}/`,
-        feed_url: `https://www.aqraaz.com/${lang}/rss.xml`,
+        id: `https://www.aqraaz.com/en/`,
+        link: `https://www.aqraaz.com/en/`,
+        site_url:  `https://www.aqraaz.com/en/`,
+        feed_url: `https://www.aqraaz.com/en/rss.xml`,
         image_url: `https://www.aqraaz.com/favicon.ico`,
         pubDate: new Date(),
         copyright: `All right reserved ${new Date().getFullYear()}`
