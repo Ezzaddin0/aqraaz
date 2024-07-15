e8e641692004ee23bd32d11864f1d9a9ea40bfa3
Ezzaddin0 <ezoozm24@gmail.com>
2024-07-16 00:44:36 +0300

    fix bag from api views

--- app/api/views/route.js
+++ app/api/views/route.js
@@ -2,6 +2,35 @@ import prisma from "../../connect"
 import { NextResponse } from "next/server";
 
 // GET VIEW COUNT FOR A POST
+// export const GET = async (req, { params }) => {
+//   const { slug } = params;
+
+//   try {
+//     const post = await prisma.post.findUnique({
+//       where: { slug },
+//     });
+
+//     if (!post) {
+//       return new NextResponse(
+//         JSON.stringify({ message: "Post not found!" }, { status: 404 })
+//       );
+//     }
+
+//     const viewCount = await prisma.view.count({
+//       where: {
+//         postId: post.id,
+//       },
+//     });
+
+//     return new NextResponse(JSON.stringify({ viewCount }, { status: 200 }));
+//   } catch (err) {
+//     console.log(err);
+//     return new NextResponse(
+//       JSON.stringify({ message: "Something went wrong!" }, { status: 500 })
+//     );
+//   }
+// };
+
 export const GET = async (req, { params }) => {
   const { slug } = params;
 
@@ -11,9 +40,7 @@ export const GET = async (req, { params }) => {
     });
 
     if (!post) {
-      return new NextResponse(
-        JSON.stringify({ message: "Post not found!" }, { status: 404 })
-      );
+      return NextResponse.json({ message: "Post not found!" }, { status: 404 });
     }
 
     const viewCount = await prisma.view.count({
@@ -22,11 +49,9 @@ export const GET = async (req, { params }) => {
       },
     });
 
-    return new NextResponse(JSON.stringify({ viewCount }, { status: 200 }));
+    return NextResponse.json({ viewCount }, { status: 200 });
   } catch (err) {
     console.log(err);
-    return new NextResponse(
-      JSON.stringify({ message: "Something went wrong!" }, { status: 500 })
-    );
+    return NextResponse.json({ message: "Something went wrong!" }, { status: 500 });
   }
 };
