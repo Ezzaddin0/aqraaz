e7f8a6a3a0bb576d25a00d17aeef6788ee27d49b
Ezzaddin0 <ezoozm24@gmail.com>
2024-07-22 23:58:46 +0300

    fix sitemap

--- app/[locale]/sitemap.ts
+++ app/[locale]/sitemap.ts
@@ -9,35 +9,17 @@ export default async function sitemap() {
     const BaseUrlEn = "https://www.aqraaz.com/en";
     const BaseUrlAr = "https://www.aqraaz.com/ar";
 
-
-    // const query = groq`
-    // *[_type == "post"]`
-
-    // const posts = await client.fetch(query);
-
     const posts = await getPosts();
-    const categories = await getCategories();
+    const categories = await getCategories();        
 
     const postUrlsEnSlug = posts.posts.map((post:any) => ({
         url: `${BaseUrlEn}/post/${post.slug}`,
         lastModified: post?.createdAt,
     }))
-    const postUrlsArSlug = posts.map((post:any) => ({
+    const postUrlsArSlug = posts.posts.map((post:any) => ({
         url: `${BaseUrlAr}/post/${post.slug}`,
         lastModified: post?.createdAt,
     }))
-    // const postUrlsEn = posts.map((post:any) => ({
-    //     url: `${BaseUrlEn}/post/${post.slug.current}`,
-    //     lastModified: post?._createdAt,
-    // }))
-    // const postUrlsAr = posts.map((post:any) => ({
-    //     url: `${BaseUrlAr}/post/${post.slug.current}`,
-    //     lastModified: post?._createdAt,
-    // }))
-
-    // const queryCategory = groq`
-    // *[_type == 'category']`
-    // const categories = await client.fetch(queryCategory);
 
     const CategoryUrlsEn = categories.map((post:any) => ({
         url: `${BaseUrlEn}/categories/${post.slug}`,
