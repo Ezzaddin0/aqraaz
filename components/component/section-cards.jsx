9d3abf4e53f889c181e245b27b6c26e7712041b5
Ezzaddin0 <ezoozm24@gmail.com>
2024-08-28 16:06:57 +0300

    fix problem adsterra ads يارب يضبط

--- components/component/section-cards.jsx
+++ components/component/section-cards.jsx
@@ -1,10 +1,34 @@
+"use client"
 import { CardContent, Card } from "../ui/card"
 import { parseISO, compareDesc } from 'date-fns';
 import CardCustom from "./Card";
 import Script from "next/script";
+import { useEffect, useRef } from "react";
 
 export default function SectionCards({Posts, lang, time, title, page, cat}) {
   const sortedPosts = Posts.posts.slice().sort((a, b) => compareDesc(parseISO(a.createdAt), parseISO(b.createdAt)));
+
+  const banner = useRef()
+
+    const atOptions = {
+        key: '8377307cb2756e90c6a11a4f1bc0b0db',
+        format: 'iframe',
+        height: 50,
+        width: 320,
+        params: {},
+    }
+    useEffect(() => {
+    if (banner.current && !banner.current.firstChild) {
+        const conf = document.createElement('script')
+        const script = document.createElement('script')
+        script.type = 'text/javascript'
+        script.src = `//www.topcreativeformat.com/${atOptions.key}/invoke.js`
+        conf.innerHTML = `atOptions = ${JSON.stringify(atOptions)}`
+
+        banner.current.append(conf)
+        banner.current.append(script)
+    }
+}, [banner])
   
   return (
     (<section className="py-8 md:py-12 lg:py-16">
@@ -20,10 +44,10 @@ export default function SectionCards({Posts, lang, time, title, page, cat}) {
           </div>
           <div>
             <Card className="h-full">
-              <CardContent className="p-6">
+              <CardContent className="p-6" ref={banner}>
               {/* <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8893594961186089"
      crossorigin="anonymous" /> */}
-              <Script id="show-banner" strategy="afterInteractive">
+              {/* <Script id="show-banner" strategy="afterInteractive">
                 {`
                 atOptions = {
                     'key' : '8377307cb2756e90c6a11a4f1bc0b0db',
@@ -34,7 +58,7 @@ export default function SectionCards({Posts, lang, time, title, page, cat}) {
                 };
                 document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.topcreativeformat.com/8377307cb2756e90c6a11a4f1bc0b0db/invoke.js"></scr'+'ipt>');
                 `}
-                </Script>
+                </Script> */}
 
               </CardContent>
             </Card>
