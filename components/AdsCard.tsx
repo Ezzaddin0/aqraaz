d4af33c80f337dfcb88d4c58a09ec5e8b20f9c67
Ezzaddin0 <ezoozm24@gmail.com>
2024-08-28 15:51:29 +0300

    fix problem adsterra ads from stackoverflow

---
+++ components/AdsCard.tsx
@@ -0,0 +1,47 @@
+"use client"
+// import Script from "next/script";
+// import { Alert, AlertDescription } from "../components/ui/alert";
+// import { useEffect, useRef } from "react";
+
+// export default function AdsCard() {
+//   return (
+//     <Alert className="my-4 text-center">
+//       <AlertDescription>
+//         <Script
+//           strategy="lazyOnload"
+//           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8893594961186089"
+//           crossOrigin="anonymous"
+//         />
+        
+
+//       </AlertDescription>
+//     </Alert>
+//   );
+// }
+
+import { useEffect, useRef } from 'react'
+export default function AdsCard(): JSX.Element {
+    const banner = useRef<HTMLDivElement>()
+
+    const atOptions = {
+        key: '9c6fb7c9b8688fb4a40951b1740b6d67',
+        format: 'iframe',
+        height: 50,
+        width: 320,
+        params: {},
+    }
+    useEffect(() => {
+    if (banner.current && !banner.current.firstChild) {
+        const conf = document.createElement('script')
+        const script = document.createElement('script')
+        script.type = 'text/javascript'
+        script.src = `//www.highperformancedformats.com/${atOptions.key}/invoke.js`
+        conf.innerHTML = `atOptions = ${JSON.stringify(atOptions)}`
+
+        banner.current.append(conf)
+        banner.current.append(script)
+    }
+}, [banner])
+
+    return <div className="mx-2 my-5 border border-gray-200 justify-center items-center text-white text-center" ref={banner}></div>
+}
